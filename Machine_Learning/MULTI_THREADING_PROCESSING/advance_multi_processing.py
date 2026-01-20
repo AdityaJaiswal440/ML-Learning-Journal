9d0ae1be2d4c03c4cdf05fa96bd7216467727fb8
Aditya Jaiswal <jaiswalaadi193919@gmail.com>
2026-01-20 06:15:44 +0530

    Learned concept of advance_multi_processing

---
+++ Machine_Learning/MULTI_THREADING_PROCESSING/advance_multi_processing.py
@@ -0,0 +1,20 @@
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+def print_numbers(number):
+    time.sleep(1)
+    return f"Number : {number}"
+
+if __name__ == "__main__":
+    numbers = [1,2,3,4,5,6,7,8,9]
+
+    t = time.time()
+
+    with ProcessPoolExecutor(max_workers=3) as executor:
+        results = executor.map(print_numbers,numbers)
+    
+    for result in results:
+        print(result)
+    
+    finished_time = time.time() - t
+    print(finished_time)
